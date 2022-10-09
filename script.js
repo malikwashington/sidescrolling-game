bc4fcf9734d200fe704be7f04be11bd3e7bd8dee
Malik Washington <malikwashington@gmail.com>
2022-10-09 02:28:14 -0400

    parallax screen, must fix formula

--- script.js
+++ script.js
@@ -6,8 +6,66 @@ dropdown.addEventListener('change', function (e) {
 
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
-const CANVAS_WIDTH = canvas.width = 600
-const CANVAS_HEIGHT = canvas.height = 600
+const CANVAS_WIDTH = canvas.width = 800
+const CANVAS_HEIGHT = canvas.height = 700
+let gameSpeed = 15
+
+const backgroundLayer1 = new Image()
+backgroundLayer1.src= "./img/layer-1.png"
+const backgroundLayer2 = new Image()
+backgroundLayer2.src= "./img/layer-2.png"
+const backgroundLayer3 = new Image()
+backgroundLayer3.src= "./img/layer-3.png"
+const backgroundLayer4 = new Image()
+backgroundLayer4.src= "./img/layer-4.png"
+const backgroundLayer5 = new Image()
+backgroundLayer5.src = "./img/layer-5.png"
+
+class Layer {
+  constructor(image, speedModifier) {
+    this.x = 0
+    this.y = 0
+    this.width = 2400
+    this.height = 700
+    this.x2 = this.width
+    this.image = image
+    this.speedModifier = speedModifier
+    this.speed = gameSpeed * this.speedModifier
+  } 
+  update() {
+    this.speed = gameSpeed * this.speedModifier
+    if (this.x <= -this.width) {
+      this.x = this.width + this.x2 - this.speed
+    }
+    if (this.x2 <= -this.width) {
+      this.x2 = this.width + this.x - this.speed
+    }
+    this.x = Math.floor(this.x - this.speed)
+    this.x2 = Math.floor(this.x2 - this.speed)
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height)
+  }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2)
+const layer2 = new Layer(backgroundLayer2, 0.4)
+const layer3 = new Layer(backgroundLayer3, 0.6)
+const layer4 = new Layer(backgroundLayer4, 0.8)
+const layer5 = new Layer(backgroundLayer5, 1)
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
+
+function animateBackgrounds() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  gameObjects.forEach(object => {
+    object.update()
+    object.draw()
+  })
+  requestAnimationFrame(animateBackgrounds)
+}
+animateBackgrounds()
 
 const playerImage = new Image()
 playerImage.src = './img/shadow_dog.png'
@@ -27,7 +85,7 @@ const animationStates = [
   },
   {
     name: 'fall',
-    frames: 9,
+    frames: 7,
   },
   {
     name: 'run',
@@ -35,7 +93,7 @@ const animationStates = [
   },
   {
     name: 'dizzy',
-    frames: 7,
+    frames: 11,
   },
   {
     name: 'sit',
@@ -82,33 +140,7 @@ function animate() {
   gameFrame++ 
   requestAnimationFrame(animate)
 }
-animate()
+// animate()
 
-// let character = document.querySelector("#character")
-// let bolder = document.querySelector(".bolder")
-// function jump() {
-//   if (character.classList != "jump") {
-//     character.classList.add("jump")
-//   setTimeout(function () {
-//     character.classList.remove("jump")
-//   }, 500)}
-// }
 
-// function duck() {
-//   if (character.className != "jump") {
-//     character.classList.add("duck")
-//     setTimeout(function () {
-//       character.classList.remove("duck");
-//     }, 300);
-//   }
-// }
-
-// document.addEventListener("keydown", e => {
-//   switch (e.keyCode) {
-//     case 38: jump();
-//       break;
-//     case 40: duck();
-//       break;
-//   }
-// })
 
