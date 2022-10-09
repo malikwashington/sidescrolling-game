6048f4f533d1889400a79a00c68156d8df3fbe92
Malik Washington <malikwashington@gmail.com>
2022-10-09 03:42:26 -0400

    taking a break to watch anime

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 let playerState = "idle"
-const dropdown = document.getElementById('animations')
-dropdown.addEventListener('change', function (e) {
-  playerState = e.target.value
-})
-
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
-let gameSpeed = 15
+let gameSpeed = 5
+let gameFrame = 0;
 
 const backgroundLayer1 = new Image()
 backgroundLayer1.src= "./img/layer-1.png"
@@ -21,33 +17,34 @@ backgroundLayer4.src= "./img/layer-4.png"
 const backgroundLayer5 = new Image()
 backgroundLayer5.src = "./img/layer-5.png"
 
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0
-    this.y = 0
-    this.width = 2400
-    this.height = 700
-    this.x2 = this.width
-    this.image = image
-    this.speedModifier = speedModifier
-    this.speed = gameSpeed * this.speedModifier
-  } 
-  update() {
-    this.speed = gameSpeed * this.speedModifier
-    if (this.x <= -this.width) {
-      this.x = this.width + this.x2 - this.speed
+window.addEventListener('load', function () {
+  const slider = document.querySelector('#slider')
+  slider.value = gameSpeed
+  const showGameSpeed = document.getElementById('showGameSpeed')
+  showGameSpeed.innerHTML = gameSpeed
+  slider.addEventListener('change', function (e) {
+    gameSpeed = e.target.value
+    showGameSpeed.innerHTML = e.target.value
+  })
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0
+      this.y = 0
+      this.width = 2400
+      this.height = 700
+      this.image = image
+      this.speedModifier = speedModifier
+      this.speed = gameSpeed * this.speedModifier
     }
-    if (this.x2 <= -this.width) {
-      this.x2 = this.width + this.x - this.speed
+    update() {
+      this.speed = gameSpeed * this.speedModifier
+      this.x = gameFrame * this.speed % this.width
     }
-    this.x = Math.floor(this.x - this.speed)
-    this.x2 = Math.floor(this.x2 - this.speed)
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+      ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height)
+    }
   }
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
-    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height)
-  }
-}
 
 const layer1 = new Layer(backgroundLayer1, 0.2)
 const layer2 = new Layer(backgroundLayer2, 0.4)
@@ -63,15 +60,15 @@ function animateBackgrounds() {
     object.update()
     object.draw()
   })
+  gameFrame--
   requestAnimationFrame(animateBackgrounds)
 }
-animateBackgrounds()
+animateBackgrounds()})
 
 const playerImage = new Image()
 playerImage.src = './img/shadow_dog.png'
 const spriteWidth = 575
 const spriteHeight = 523
-let gameFrame = 0
 const staggerFrames = 5
 const spriteAnimations = []
 const animationStates = [
@@ -129,7 +126,7 @@ animationStates.forEach((state, index) => {
 })
 
 function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  // ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
   let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length
   let frameX = spriteWidth * position
   let frameY= spriteAnimations[playerState].loc[position].y
